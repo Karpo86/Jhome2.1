d0f1e1137c8aecefef5b0a9e73d5a4f39b14a50e
Karpo86 <140810499+Karpo86@users.noreply.github.com>
2023-09-07 18:10:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-public class Main { public static void main(String[] args) {
-        int ticket_cost = 17530;
-        int miles_math = 20;
-        System.out.println("Начислено "+ ticket_cost/miles_math + " бонусов" );
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 17530;
+        int milesMath = 20;
+        System.out.println("Начислено " + ticketCost / milesMath + " бонусов");
     }
 }
